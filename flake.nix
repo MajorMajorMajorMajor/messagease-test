f7c4dce1a60d360ac0ea859c10e1c36c9d2d87b4
Stas <stas@local>
2026-09-02 22:20:21 +0000

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "messagease-test for Pebble";
+
+  inputs = {
+    pebble.url = "github:pebble-dev/pebble.nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { pebble, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell = pebble.pebbleEnv.${system} { };
+    });
+}
