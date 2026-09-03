27370c108c71a8f8e72e7297d0128dc9dac16564
Stas <stas@local>
2026-09-03 18:29:03 +0000

    Sample app from latest sdk

--- src/c/main.c
+++ src/c/main.c
@@ -1,27 +1,27 @@
 #include <pebble.h>
 
-static Window *s_main_window;
+static Window *s_window;
 static TextLayer *s_text_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_text_layer, "Select");
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_text_layer, "Up");
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_text_layer, "Down");
 }
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+static void prv_click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
 }
 
-static void main_window_load(Window *window) {
+static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
@@ -31,26 +31,30 @@ static void main_window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
 
-static void main_window_unload(Window *window) {
+static void prv_window_unload(Window *window) {
   text_layer_destroy(s_text_layer);
 }
 
-static void init(void) {
-  s_main_window = window_create();
-  window_set_click_config_provider(s_main_window, click_config_provider);
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload,
+static void prv_init(void) {
+  s_window = window_create();
+  window_set_click_config_provider(s_window, prv_click_config_provider);
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = prv_window_load,
+    .unload = prv_window_unload,
   });
-  window_stack_push(s_main_window, true);
+  const bool animated = true;
+  window_stack_push(s_window, animated);
 }
 
-static void deinit(void) {
-  window_destroy(s_main_window);
+static void prv_deinit(void) {
+  window_destroy(s_window);
 }
 
 int main(void) {
-  init();
+  prv_init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
+
   app_event_loop();
-  deinit();
+  prv_deinit();
 }
