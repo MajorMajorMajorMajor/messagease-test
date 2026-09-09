a53f48f1fb391b9525b4966d2587e38b0fde5e44
Stas <stas@local>
2026-09-09 19:45:22 +0000

    Add comments

--- src/c/alert.h
+++ src/c/alert.h
@@ -3,10 +3,14 @@
 
 #include <pebble.h>
 
+// limits
 enum {
     ALERT_MAX_LENGTH = 200
 };
+
+// show an alert
 void alert_set_text(const char *new_text);
 
+// functions to build and break down the ui
 TextLayer* alert_ui_init(Layer *parent_layer, GRect frame);
 void alert_ui_deinit();
