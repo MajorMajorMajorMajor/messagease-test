ca89d44062aeb3afeb79b36e16d64d17c99ee30b
Stas <stas@local>
2026-09-11 15:54:00 +0000

    Pass around layout IDs instead of layout pointers

--- src/c/ui/key_grid.c
+++ src/c/ui/key_grid.c
@@ -3,25 +3,65 @@
 #include "keyboard/layout.h"
 
 #include "ui/ui.h"
+#include "ui/key_button.h"
 
 // bind together the physical layer object with the layout it's supposed to represent
-typedef struct KeyGrid {
-  Layer layer;
-  Layout layout;
-}
+typedef struct {
+  Layer *layer;
+  const Layout *layout;
+} KeyGrid;
 
 // one grid per layout
-static Layer *s_grid_layers[LAYOUT_COUNT];
+static KeyGrid s_key_grids[LAYOUT_COUNT];
 
-static Layer *prv_make_grid_layer(Layout L, UIDimensions ui){
-  
+// create a key grid
+static void prv_create(Layer *parent_layer, LayoutId layout_id) {    
+  // Layer
+  GRect frame = layer_get_bounds(parent_layer);
+  Layer *grid_layer = layer_create(frame);
+
+  // Layout
+  const Layout *layout = get_layout(layout_id);
+
+  // Instantiate grid
+  s_key_grids[layout_id] = (KeyGrid){.layer = grid_layer, .layout=layout};
+
+  // Create buttons
+
+  // Add layers to the hierarchy
+  layer_add_child(parent_layer, grid_layer);
 }
 
+static void prv_destroy(LayoutId layout_id) {      
+  // destroy layer
+  Layer *layer = s_key_grids[layout_id].layer;
+  layer_destroy(layer);
+}
+
+// shows the grid for `layout_id` and hides the rest
+static void prv_show(LayoutId layout_id) {  
+  KeyGrid *grid;
+
+  for (LayoutId i = 0; i < LAYOUT_COUNT; i++) {
+    grid = &s_key_grids[i];
+
+    layer_set_hidden(grid->layer, layout_id != i); // hide non-matching layouts
+  }
+}
 
 void key_grid_ui_init(Layer *parent_layer, UIDimensions ui) {    
-  const Layout *initial_layout = keyboard_get_active_layout();
+  // create grids for all layouts
+  for (LayoutId i = 0; i < LAYOUT_COUNT; i++) {
+    prv_create(parent_layer, i);
+  }
 
-  grid_layer = prv_make_grid_layer(initial_layout, ui);
+  // show the initial layout
+  LayoutId initial_layout_id = keyboard_get_active_layout_id();
+  prv_show(initial_layout_id);
+}
 
-  initial_layout->keys;
+void key_grid_ui_deinit() {      
+  for (LayoutId i = 0; i < LAYOUT_COUNT; i++) {
+    prv_destroy(i);
+  }
 }
