647a7dab5aee82366a2775c40154376ced565396
Stas <stas@local>
2026-09-11 15:09:14 +0000

    Don't compute grids in ui.c

--- src/c/ui/ui.c
+++ src/c/ui/ui.c
@@ -1,27 +1,34 @@
 #include <pebble.h>
+#include "keyboard/layout.h"
+
 #include "ui.h"
+
+
 
 // static UIDimensions s_ui_dimensions;
 
 UIDimensions layout_compute_ui_dimensions(GSize size) {
-  int textbox_height = 20;
-
-  int cell_width = size.w / 4;
-  int cell_height = (size.h - textbox_height) / 4;
+  // Size the textbox
+  int textbox_width = size.w;  
+  int textbox_height = 40;   
 
   GSize textbox_size = {
-    .w = size.w, 
+    .w = textbox_width, 
     .h = textbox_height 
   };
 
-  GSize cell_size = {
-    .w = cell_width,
-    .h = cell_height
+
+  // Remaining vertical space taken up by key grid
+  int key_grid_width = size.w;
+  int key_grid_height = size.h - textbox_height;
+  GSize key_grid_size = {
+    .w = key_grid_width,
+    .h = key_grid_height
   };
 
   return (UIDimensions) {
     .textbox_size = textbox_size,
-    .cell_size = cell_size
+    .key_grid_size = key_grid_size
   };
 }
 
