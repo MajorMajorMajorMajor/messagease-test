466da3d137a7e29e5cdda814acf02510bc22cfb6
Stas <stas@local>
2026-09-14 15:31:59 -0400

    work on key label logic

--- src/c/keyboard/key_label.h
+++ src/c/keyboard/key_label.h
@@ -14,6 +14,7 @@ typedef enum {
 } KeyLabelType;
 
 typedef struct {  
+  KeyLabelSlot slot;
   KeyLabelType type;
   union {
     char *text;
--- src/c/ui/key_button.c
+++ src/c/ui/key_button.c
@@ -17,18 +17,32 @@ struct KeyButton {
 
 
 typedef enum {
-  LABEL_TYPE_TEXT,
   LABEL_TYPE_IMAGE
 } LabelType;
 
 
 
 
-static void prv_label_create(KeyButton *this, KeyLabelSlot pos, char *label_text){
-  const Layer *base_layer = this->base_layer;
-  GRect frame = layer_get_bounds(base_layer);
+static void prv_draw_key_label(Layer *layer, GContext ctx) {
 
   Layer *label_layer = layer_create(frame);
+  // draw the center label text
+  char *text = key->center_label;
+  // const GFont font = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK); // @ TODO: Make the font and colors configurable
+  const GFont font = fonts_get_system_font(FONT_KEY_LECO_26_BOLD_NUMBERS_AM_PM); // @ TODO: Make the font and colors configurable
+  
+  GTextOverflowMode overflow_mode = GTextOverflowModeWordWrap;
+  GTextAlignment alignment = GTextAlignmentCenter;  
+  
+
+  GSize text_size = graphics_text_layout_get_content_size(text, font, box, overflow_mode, alignment);  
+  int inset_top = (bounds.size.h - text_size.h)/3;
+  GRect box_vcenter = grect_inset(bounds, GEdgeInsets(inset_top));
+
+  graphics_context_set_text_color(ctx, GColorBlack);
+  graphics_draw_text(ctx, text, font, box_vcenter, overflow_mode, alignment, NULL);
+  
+
 
 
 
@@ -36,6 +50,7 @@ static void prv_label_create(KeyButton *this, KeyLabelSlot pos, char *label_text
 
 KeyButton *key_button_create(Layer *parent_layer, const Key *key){    
     Layer *base_layer = layer_create(layer_get_bounds(parent_layer));
+    GRect frame = layer_get_bounds(base_layer);
   
     KeyButton *key_button = malloc(sizeof *key_button);
     *key_button = (KeyButton){
@@ -65,23 +80,7 @@ static void prv_update_key_layer(struct Layer *layer, GContext* ctx){
   graphics_context_set_fill_color(ctx, GColorRichBrilliantLavender);
   graphics_fill_rect(ctx, box, 4, GCornersAll);
 
-  // draw the center label text
-  char *text = key->center_label;
-  // const GFont font = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK); // @ TODO: Make the font and colors configurable
-  const GFont font = fonts_get_system_font(FONT_KEY_LECO_26_BOLD_NUMBERS_AM_PM); // @ TODO: Make the font and colors configurable
-  
-  GTextOverflowMode overflow_mode = GTextOverflowModeWordWrap;
-  GTextAlignment alignment = GTextAlignmentCenter;  
-  
-
-  GSize text_size = graphics_text_layout_get_content_size(text, font, box, overflow_mode, alignment);  
-  int inset_top = (bounds.size.h - text_size.h)/3;
-  GRect box_vcenter = grect_inset(bounds, GEdgeInsets(inset_top));
-
-  graphics_context_set_text_color(ctx, GColorBlack);
-  graphics_draw_text(ctx, text, font, box_vcenter, overflow_mode, alignment, NULL);
-  
-
+  // print label text
   return;
 }
 
