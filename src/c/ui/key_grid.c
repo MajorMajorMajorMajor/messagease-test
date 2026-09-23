adafcbfbd5a6bfa059025593d3397de23baffcf9
Stas <stas@local>
2026-09-23 01:05:32 +0000

    wip

--- src/c/ui/key_grid.c
+++ src/c/ui/key_grid.c
@@ -6,7 +6,7 @@
 #include "ui/key_button.h"
 
 // Create a layer to hold buttons for each key in the layout
-typedef struct {  
+typedef struct {
   const Layout *key_layout;
   Layer *layer;
   KeyButton *ui_key_buttons[]; // array size: key_layout->key_count
@@ -17,43 +17,51 @@ enum { KEY_GRID_COUNT = LAYOUT_COUNT };
 static KeyGrid s_key_grids[KEY_GRID_COUNT];
 
 // create a key grid
-static void prv_create(const Layer *parent_layer, const Layout *layout) {    
+static void prv_create(const Layer *parent_layer, const Layout *layout) {
   // Layer
   GRect frame = layer_get_bounds(parent_layer);
   Layer *grid_layer = layer_create(frame);
 
+  // divide the frame width evenly among the grid cells
+  int cell_width  = (float) frame.size.w / layout->cols;
+  int cell_height = (float) frame.size.h / layout->rows;
+
+
   // Create buttons
-  size_t i;  
+  size_t i;
   for (i = 0; i < layout->key_count; i++) {
     const LayoutKeyPosition *pos = &layout->keys[i];
-    ib
-    const unsigned int button_width = 12;
-    
-    int x = pos->row * button_width;
-    int y = 0;
-    int w = 0;
-    int h = 0;
+
+    int x = cell_width  * pos->col;
+    int y = cell_height * pos->row;
+    int w = cell_width  * pos->col_span;
+    int h = cell_height * pos->row_span;
 
     GRect button_frame = GRect(x, y, w, h);
 
-    
+    const Key *key = get_key(pos->key_id);
+    KeyButton kb = key_button_create(key, button_frame);
+
+    // s_key_grids
+
+
 
   };
 
-  
+
 
   // Add layers to the hierarchy
   layer_add_child(parent_layer, grid_layer);
 }
 
-static void prv_destroy(LayoutId layout_id) {      
+static void prv_destroy(LayoutId layout_id) {
   // destroy layer
   Layer *layer = s_key_grids[layout_id].layer;
   layer_destroy(layer);
 }
 
 // shows the grid for `layout_id` and hides the rest
-static void prv_show(LayoutId layout_id) {  
+static void prv_show(LayoutId layout_id) {
   KeyGrid *grid;
 
   for (LayoutId i = 0; i < LAYOUT_COUNT; i++) {
@@ -63,7 +71,7 @@ static void prv_show(LayoutId layout_id) {
   }
 }
 
-void key_grid_ui_init(Layer *parent_layer, UIDimensions ui) {    
+void key_grid_ui_init(Layer *parent_layer, UIDimensions ui) {
   // create grids for all layouts
   for (LayoutId i = 0; i < LAYOUT_COUNT; i++) {
     prv_create(parent_layer, i);
@@ -74,7 +82,7 @@ void key_grid_ui_init(Layer *parent_layer, UIDimensions ui) {
   prv_show(initial_layout_id);
 }
 
-void key_grid_ui_deinit() {      
+void key_grid_ui_deinit() {
   for (LayoutId i = 0; i < LAYOUT_COUNT; i++) {
     prv_destroy(i);
   }
